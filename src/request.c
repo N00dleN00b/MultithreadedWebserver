6180a64d386c1c849a5a03600c525c5fc7049fa0
N00dleN00b <nbr6901@utulsa.edu>
2025-05-03 10:28:46 -0500

    I accidentally did the new code in request.c instead of webserver; reverting changes now

--- src/request.c
+++ src/request.c
@@ -1,254 +1,207 @@
 #include "io_helper.h"
 #include "request.h"
 
-#define MAXBUF (8192)
-
-//	TODO: add code to create and manage the buffer
-
 #include <pthread.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <time.h>
 
-// Incoming HTTP Request Pending Structure
-typedef struct request {
-    int fd;                   
-    char filename[1024];        
-    // Time when the request was received
-    time_t arrival_time;        
-    // Ptr to next request
-    struct request* next;       
+#define MAXBUF (8192)
+
+// Scheduler config (from request.h)
+int num_threads = DEFAULT_THREADS;
+int buffer_max_size = DEFAULT_BUFFER_SIZE;
+int scheduling_algo = DEFAULT_SCHED_ALGO;
+
+typedef struct {
+    int fd;
+    int filesize;
 } request_t;
 
-// Managin the buffer - w/global variables 
-request_t* request_head = NULL;    
-// Mutex to protect the request queue                     
-pthread_mutex_t buffer_lock = PTHREAD_MUTEX_INITIALIZER; 
-// Conditional var to signal
-pthread_cond_t buffer_not_empty = PTHREAD_COND_INITIALIZER; 
+request_t request_buffer[DEFAULT_BUFFER_SIZE];
+int buffer_start = 0, buffer_end = 0, buffer_count = 0;
 
-//
-// Sends out HTTP response in case of errors
-//
+pthread_mutex_t buffer_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t buffer_not_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t buffer_not_full = PTHREAD_COND_INITIALIZER;
+
 void request_error(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
     char buf[MAXBUF], body[MAXBUF];
+
+    sprintf(body, "<!doctype html>\r\n"
+                  "<head><title>WebServer Error</title></head>\r\n"
+                  "<body><h2>%s: %s</h2><p>%s: %s</p></body>\r\n</html>\r\n",
+            errnum, shortmsg, longmsg, cause);
     
-    // Create the body of error message first (have to know its length for header)
-    sprintf(body, ""
-	    "<!doctype html>\r\n"
-	    "<head>\r\n"
-	    "  <title>CYB-3053 WebServer Error</title>\r\n"
-	    "</head>\r\n"
-	    "<body>\r\n"
-	    "  <h2>%s: %s</h2>\r\n" 
-	    "  <p>%s: %s</p>\r\n"
-	    "</body>\r\n"
-	    "</html>\r\n", errnum, shortmsg, longmsg, cause);
-    
-    // Write out the header information for this response
     sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
     write_or_die(fd, buf, strlen(buf));
-    
     sprintf(buf, "Content-Type: text/html\r\n");
     write_or_die(fd, buf, strlen(buf));
-    
     sprintf(buf, "Content-Length: %lu\r\n\r\n", strlen(body));
     write_or_die(fd, buf, strlen(buf));
-    
-    // Write out the body last
     write_or_die(fd, body, strlen(body));
-    
-    // close the socket connection
     close_or_die(fd);
 }
 
-//
-// Reads and discards everything up to an empty text line
-//
 void request_read_headers(int fd) {
     char buf[MAXBUF];
-    
     readline_or_die(fd, buf, MAXBUF);
     while (strcmp(buf, "\r\n")) {
-		readline_or_die(fd, buf, MAXBUF);
+        readline_or_die(fd, buf, MAXBUF);
     }
-    return;
 }
 
-//
-// Return 1 if static, 0 if dynamic content (executable file)
-// Calculates filename (and cgiargs, for dynamic) from uri
-//
 int request_parse_uri(char *uri, char *filename, char *cgiargs) {
     char *ptr;
-    
-    if (!strstr(uri, "cgi")) { 
-	// static
-	strcpy(cgiargs, "");
-	sprintf(filename, ".%s", uri);
-	if (uri[strlen(uri)-1] == '/') {
-	    strcat(filename, "index.html");
-	}
-	return 1;
-    } else { 
-	// dynamic
-	ptr = index(uri, '?');
-	if (ptr) {
-	    strcpy(cgiargs, ptr+1);
-	    *ptr = '\0';
-	} else {
-	    strcpy(cgiargs, "");
-	}
-	sprintf(filename, ".%s", uri);
-	return 0;
+    if (!strstr(uri, "cgi")) {
+        strcpy(cgiargs, "");
+        sprintf(filename, ".%s", uri);
+        if (uri[strlen(uri)-1] == '/')
+            strcat(filename, "index.html");
+        return 1;
+    } else {
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr + 1);
+            *ptr = '\0';
+        } else {
+            strcpy(cgiargs, "");
+        }
+        sprintf(filename, ".%s", uri);
+        return 0;
     }
 }
 
-//
-// Fills in the filetype given the filename
-//
 void request_get_filetype(char *filename, char *filetype) {
-    if (strstr(filename, ".html")) 
-		strcpy(filetype, "text/html");
-    else if (strstr(filename, ".gif")) 
-		strcpy(filetype, "image/gif");
-    else if (strstr(filename, ".jpg")) 
-		strcpy(filetype, "image/jpeg");
-    else 
-		strcpy(filetype, "text/plain");
+    if (strstr(filename, ".html")) strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif")) strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".jpg")) strcpy(filetype, "image/jpeg");
+    else strcpy(filetype, "text/plain");
 }
 
-//
-// Handles requests for static content
-//
 void request_serve_static(int fd, char *filename, int filesize) {
     int srcfd;
     char *srcp, filetype[MAXBUF], buf[MAXBUF];
-    
+
     request_get_filetype(filename, filetype);
     srcfd = open_or_die(filename, O_RDONLY, 0);
-    
-    // Rather than call read() to read the file into memory, 
-    // which would require that we allocate a buffer, we memory-map the file
     srcp = mmap_or_die(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
     close_or_die(srcfd);
-    
-    // put together response
-    sprintf(buf, ""
-	    "HTTP/1.0 200 OK\r\n"
-	    "Server: OSTEP WebServer\r\n"
-	    "Content-Length: %d\r\n"
-	    "Content-Type: %s\r\n\r\n", 
-	    filesize, filetype);
-       
+
+    sprintf(buf, "HTTP/1.0 200 OK\r\n"
+                 "Server: Multithreaded WebServer\r\n"
+                 "Content-Length: %d\r\n"
+                 "Content-Type: %s\r\n\r\n",
+            filesize, filetype);
+
     write_or_die(fd, buf, strlen(buf));
-    
-    //  Writes out to the client socket the memory-mapped file 
     write_or_die(fd, srcp, filesize);
     munmap_or_die(srcp, filesize);
 }
 
-//
-// Fetches the requests from the buffer and handles them (thread logic)
-//
-void* thread_request_serve_static(void* arg)
-{
-	// TODO: write code to actualy respond to HTTP requests
-  while (1) {
-    // Fetch request from the buffer 
-    request_t* req = fetch_request();
-    // if buffer empty, wait
-    if (req != NULL) {
-        struct stat sbuf;
-        
-        // ensures requested file is still working/accessible
-        if (stat(req->filename, &sbuf) < 0) {
-            // If file not found, send 404 error
-            request_error(req->fd, req->filename, "404", "Not found", "server could not find this file");
-        } else {
-            // otherwise serve the static file to client
-            request_serve_static(req->fd, req->filename, sbuf.st_size);
+void* thread_request_serve_static(void* arg) {
+    srand(time(NULL) ^ pthread_self()); // Seed randomness
+
+    while (1) {
+        pthread_mutex_lock(&buffer_lock);
+        while (buffer_count == 0)
+            pthread_cond_wait(&buffer_not_empty, &buffer_lock);
+
+        int index = buffer_start;
+        if (scheduling_algo == 2) {
+            // Random
+            int r = rand() % buffer_count;
+            index = (buffer_start + r) % buffer_max_size;
         }
-        
-        // close client connection
-        close_or_die(req->fd);  
-        free(req);              
+
+        request_t req = request_buffer[index];
+
+        // Remove request
+        if (index == buffer_start) {
+            buffer_start = (buffer_start + 1) % buffer_max_size;
+        } else {
+            for (int i = index; i != buffer_start; i = (i - 1 + buffer_max_size) % buffer_max_size) {
+                request_buffer[i] = request_buffer[(i - 1 + buffer_max_size) % buffer_max_size];
+            }
+            buffer_start = (buffer_start + 1) % buffer_max_size;
+        }
+
+        buffer_count--;
+        pthread_cond_signal(&buffer_not_full);
+        pthread_mutex_unlock(&buffer_lock);
+
+        request_handle(req.fd);
+        close_or_die(req.fd);
     }
 }
-return NULL;
 
-}
-
-//
-// Initial handling of the request
-//
 void request_handle(int fd) {
     int is_static;
     struct stat sbuf;
     char buf[MAXBUF], method[MAXBUF], uri[MAXBUF], version[MAXBUF];
     char filename[MAXBUF], cgiargs[MAXBUF];
-    
-	// get the request type, file path and HTTP version
+
     readline_or_die(fd, buf, MAXBUF);
     sscanf(buf, "%s %s %s", method, uri, version);
     printf("method:%s uri:%s version:%s\n", method, uri, version);
 
-	// verify if the request type is GET or not
     if (strcasecmp(method, "GET")) {
-		request_error(fd, method, "501", "Not Implemented", "server does not implement this method");
-		return;
+        request_error(fd, method, "501", "Not Implemented", "server does not implement this method");
+        return;
     }
+
     request_read_headers(fd);
-    
-	// check requested content type (static/dynamic)
     is_static = request_parse_uri(uri, filename, cgiargs);
-    
-	// get some data regarding the requested file, also check if requested file is present on server
+
     if (stat(filename, &sbuf) < 0) {
-		request_error(fd, filename, "404", "Not found", "server could not find this file");
-		return;
+        request_error(fd, filename, "404", "Not found", "file not found on server");
+        return;
     }
-    
-	// verify if requested content is static
+
     if (is_static) {
-		if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-			request_error(fd, filename, "403", "Forbidden", "server could not read this file");
-			return;
-		}
-		
-		// TODO: write code to add HTTP requests in the buffer based on the scheduling policy
-    // Add a new HTTP request into the buffer
-    void add_request(int fd, const char* filename) {
-      request_t* new_req = malloc(sizeof(request_t));
-      if (!new_req) {
-          perror("malloc");
-          exit(1);
-      }
-      new_req->fd = fd;
-      strncpy(new_req->filename, filename, sizeof(new_req->filename) - 1);
-      new_req->arrival_time = time(NULL);
-      new_req->next = NULL;
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+            request_error(fd, filename, "403", "Forbidden", "cannot read this file");
+            return;
+        }
 
-      pthread_mutex_lock(&buffer_lock);
+        if (strstr(filename, "..")) {
+            request_error(fd, filename, "403", "Forbidden", "directory traversal attempt blocked");
+            return;
+        }
 
-      // Insert at the end of the linked list (FIFO insert)
-      // FIFO/SFF/Random applied later during fetch_request()
-      if (request_head == NULL) {
-          request_head = new_req;
-      } else {
-          request_t* curr = request_head;
-          while (curr->next != NULL) {
-              curr = curr->next;
-          }
-          curr->next = new_req;
-      }
+        pthread_mutex_lock(&buffer_lock);
+        while (buffer_count == buffer_max_size)
+            pthread_cond_wait(&buffer_not_full, &buffer_lock);
 
-      // Wake up 
-      pthread_cond_signal(&buffer_not_empty); 
-      pthread_mutex_unlock(&buffer_lock);
-    }
+        request_t new_request = { fd, sbuf.st_size };
 
+        if (scheduling_algo == 0 || scheduling_algo == 2) {
+            // FIFO or Random
+            request_buffer[buffer_end] = new_request;
+            buffer_end = (buffer_end + 1) % buffer_max_size;
+        } else if (scheduling_algo == 1) {
+            // SFF: insert sorted by filesize
+            int i = buffer_count - 1;
+            int idx = (buffer_start + i) % buffer_max_size;
+            while (i >= 0 && request_buffer[idx].filesize > new_request.filesize) {
+                request_buffer[(buffer_start + i + 1) % buffer_max_size] = request_buffer[idx];
+                i--;
+                idx = (idx - 1 + buffer_max_size) % buffer_max_size;
+            }
+            request_buffer[(buffer_start + i + 1) % buffer_max_size] = new_request;
+            buffer_end = (buffer_start + buffer_count + 1) % buffer_max_size;
+        }
+
+        buffer_count++;
+        pthread_cond_signal(&buffer_not_empty);
+        pthread_mutex_unlock(&buffer_lock);
+        return;
     } else {
-		request_error(fd, filename, "501", "Not Implemented", "server does not serve dynamic content request");
+        request_error(fd, filename, "501", "Not Implemented", "dynamic content not supported");
     }
 }
